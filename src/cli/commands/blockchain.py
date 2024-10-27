f5761754b65f6b9330dd85cb77ae112aae5aeaca
KOSASIH <kosasihg88@gmail.com>
2024-10-27 19:24:17 +0700

    Create blockchain.py

---
+++ src/cli/commands/blockchain.py
@@ -0,0 +1,6 @@
+def view_blockchain():
+    """View the current state of the blockchain."""
+    # Here you would implement the logic to retrieve and display the blockchain
+    print("Current Blockchain:")
+    # For demonstration, we will just print a placeholder
+    print("[Block 1] -> [Block 2] -> [Block 3]")  # Replace with actual blockchain data
