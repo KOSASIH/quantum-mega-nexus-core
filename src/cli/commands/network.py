912822ac3dc04d0b0aa81abe6e5e193a531f8b95
KOSASIH <kosasihg88@gmail.com>
2024-10-27 19:24:50 +0700

    Create network.py

---
+++ src/cli/commands/network.py
@@ -0,0 +1,5 @@
+def add_peer(host, port):
+    """Add a new peer to the network."""
+    # Here you would implement the logic to add a peer to the network
+    print(f"Peer added: {host}:{port}")
+    # You might want to update the peer list and notify other nodes
