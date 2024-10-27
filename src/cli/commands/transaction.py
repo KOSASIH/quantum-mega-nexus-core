f02868ddcfc1a038fc4a8d7fdf072425e77488c5
KOSASIH <kosasihg88@gmail.com>
2024-10-27 19:23:32 +0700

    Create transaction.py

---
+++ src/cli/commands/transaction.py
@@ -0,0 +1,5 @@
+def create_transaction(sender, receiver, amount):
+    """Create a new transaction."""
+    # Here you would implement the logic to create a transaction
+    print(f"Transaction created: {amount} from {sender} to {receiver}")
+    # You might want to add this transaction to a pool or broadcast it to the network
